089e3fedc7db87486d80cfc73399f096c210f664
Tom Ashworth <tashworth@twitter.com>
2016-02-01 16:05:47 +0000

    feat: displayName and getDescription

--- src/createClass.js
+++ src/createClass.js
@@ -1,1 +1,20 @@
-export default function createClass() {}
+export default function createClass(spec) {
+    const {
+        displayName = '',
+        getDescription
+    } = (spec || {});
+
+    class Action {
+        static displayName = displayName;
+        displayName = displayName;
+        getDescription() {
+            return (
+                typeof getDescription === 'function'
+                    ? getDescription.call(this)
+                    : this.displayName
+            );
+        }
+    }
+
+    return Action;
+}
--- test/createClass.test.js
+++ test/createClass.test.js
@@ -4,3 +4,57 @@ import createClass from '../src/createClass';
 test('importable', (t) => {
     t.ok(createClass);
 });
+
+test('createClass handles no spec', (t) => {
+    t.ok(createClass());
+});
+
+test('createClass has default static displayName', (t) => {
+    t.same(createClass({}).displayName, '');
+});
+
+test('createClass has default instance displayName', (t) => {
+    var i = new createClass({});
+    t.same(i.displayName, '');
+});
+
+test('createClass copies displayName to static property', (t) => {
+    t.same(
+        createClass({ displayName: 'The Example' }).displayName,
+        'The Example'
+    );
+});
+
+test('createClass copies displayName to instance property', (t) => {
+    var i = createClass({ displayName: 'The Example' });
+    t.same(
+        i.displayName,
+        'The Example'
+    );
+});
+
+test(
+    'createClass has default getDescription that returns the displayName',
+    (t) => {
+        var Example = createClass({ displayName: 'The Example' });
+        var example = new Example();
+        t.same(
+            example.getDescription(),
+            'The Example'
+        );
+    }
+);
+
+test(
+    'createClass can overwrite getDescription',
+    (t) => {
+        var Example = createClass({
+            getDescription: () => 'An action'
+        });
+        var example = new Example();
+        t.same(
+            example.getDescription(),
+            'An action'
+        );
+    }
+);
