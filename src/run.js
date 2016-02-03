c1230ece4af9cb065fbcf2a5ec01c0df75f2bccf
Tom Ashworth <tashworth@twitter.com>
2016-02-03 13:16:49 +0000

    chore: refactor run for readability

--- src/run.js
+++ src/run.js
@@ -1,32 +1,30 @@
 import buildRunPath from './buildRunPath';
 
+function makeBundleErrorInAction(action) {
+    return e => {
+        throw {
+            stack: e.stack,
+            message: e.message,
+            action
+        };
+    };
+}
+
+function makePhase(k, runPath) {
+    return () => runPath.reduce((pPrev, action) => {
+        return pPrev
+            .then(() => action[k]())
+            .catch(makeBundleErrorInAction(action));
+    }, Promise.resolve());
+}
+
 export default function run(targetAction, context) {
     const runPath = buildRunPath(targetAction).map(dep => {
         dep.context = context;
         return dep;
     });
-    const doRun = () => runPath.reduce((pPrev, action) => {
-        return pPrev
-            .then(() => action.run(context))
-            .catch(e => {
-                throw {
-                    stack: e.stack,
-                    message: e.message,
-                    action
-                };
-            });
-    }, Promise.resolve());
-    const doTeardown = () => runPath.reverse().reduce((pPrev, action) => {
-        return pPrev
-            .then(() => action.teardown(context))
-            .catch(e => {
-                throw {
-                    stack: e.stack,
-                    message: e.message,
-                    action
-                };
-            });
-    }, Promise.resolve());
+    const doRun = makePhase('run', runPath);
+    const doTeardown = makePhase('teardown', runPath.reverse());
 
     return Promise.resolve()
         .then(doRun)
