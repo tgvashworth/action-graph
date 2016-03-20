4bf9bd75a12d63f8a850a20c2ad2b69a46e72f62
Tom Ashworth <tgvashworth@gmail.com>
2016-03-20 16:05:28 +0000

    chore: use shorthand for run path tests

--- test/buildRunPath.test.js
+++ test/buildRunPath.test.js
@@ -4,33 +4,40 @@ import buildRunPath from '../src/buildRunPath';
 import sameActionsList from '../src/sameActionsList';
 import createClass from '../src/createClass';
 
+function cC(name, deps=[]) {
+    return createClass({
+        displayName: name,
+        getDependencies: () => deps
+    });
+}
+
 // Create a very arbitrary, complex graph
-const A = createClass({ displayName: 'A' });
-const B = createClass({ displayName: 'B', getDependencies: () => ([ new A() ]) });
-const C = createClass({ displayName: 'C', getDependencies: () => ([ new A() ]) });
-const D = createClass({ displayName: 'D', getDependencies: () => ([ new B() ]) });
-const E = createClass({ displayName: 'E', getDependencies: () => ([ new B() ]) });
-const F = createClass({ displayName: 'F', getDependencies: () => ([ new E(), new C() ]) });
-const G = createClass({ displayName: 'G', getDependencies: () => ([ new F() ]) });
-const H = createClass({ displayName: 'H', getDependencies: () => ([ new E() ]) });
-const I = createClass({ displayName: 'I', getDependencies: () => ([ new D(), new H() ]) });
-const J = createClass({ displayName: 'J', getDependencies: () => ([ new H() ]) });
-const K = createClass({ displayName: 'K' });
-const L = createClass({ displayName: 'L', getDependencies: () => ([ new K() ]) });
-const M = createClass({ displayName: 'M', getDependencies: () => ([ new K() ]) });
-const N = createClass({ displayName: 'N', getDependencies: () => ([ new L(), new M() ]) });
-const O = createClass({ displayName: 'O', getDependencies: () => ([ new J(), new G(), new N() ]) });
+const A = cC('A');
+const B = cC('B', [ new A() ]);
+const C = cC('C', [ new A() ]);
+const D = cC('D', [ new B() ]);
+const E = cC('E', [ new B() ]);
+const F = cC('F', [ new E(), new C() ]);
+const G = cC('G', [ new F() ]);
+const H = cC('H', [ new E() ]);
+const I = cC('I', [ new D(), new H() ]);
+const J = cC('J', [ new H() ]);
+const K = cC('K');
+const L = cC('L', [ new K() ]);
+const M = cC('M', [ new K() ]);
+const N = cC('N', [ new L(), new M() ]);
+const O = cC('O', [ new J(), new G(), new N() ]);
 
 // Actions that depend on constructors, not instances
-const A2 = createClass({ displayName: 'A2' });
-const B2 = createClass({ displayName: 'B2', getDependencies: () => [ A2 ] });
-const C2 = createClass({ displayName: 'C2', getDependencies: () => [ new A2(), new B2() ] });
-const D2 = createClass({ displayName: 'D2', getDependencies: () => [ new A2(), B2, new C2() ] });
-const E2 = createClass({ displayName: 'E2', getDependencies: () => [ A2, B2 ] });
-const F2 = createClass({ displayName: 'F2', getDependencies: () => [ new B2() ] });
+const A2 = cC('A2');
+const B2 = cC('B2', [ A2 ]);
+const C2 = cC('C2', [ new A2(), new B2() ]);
+const D2 = cC('D2', [ new A2(), B2, new C2() ]);
+const E2 = cC('E2', [ A2, B2 ]);
+const F2 = cC('F2', [ new B2() ]);
 // G2 is bad, its tree does not contain an implementation of A2
-const G2 = createClass({ displayName: 'G2', getDependencies: () => [ new E2(), new F2() ] });
-const H2 = createClass({ displayName: 'H2', getDependencies: () => [ new A2(), new G2() ] });
+const G2 = cC('G2', [ new E2(), new F2() ]);
+const H2 = cC('H2', [ new A2(), new G2() ]);
 
 test('importable', (t) => {
     t.ok(buildRunPath);
